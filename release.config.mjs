776cd4b555ca1ac628ae3712c54e7603baa57fbd
phishbacon <thechristianreynolds@gmail.com>
2025-02-03 12:24:45 -0500

    ci: 🎡 Add version to package.json

--- release.config.mjs
+++ release.config.mjs
@@ -16,6 +16,7 @@ export default {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
     "@semantic-release/changelog",
     "@semantic-release/git",
     "@semantic-release/github",
